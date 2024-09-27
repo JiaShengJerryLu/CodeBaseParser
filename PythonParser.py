7a41145c2cbefd191273e45a8fac3ad95473ff44
Jerry Lu <jerrylu984@gmail.com>
2024-09-26 22:39:07 -0400

    Python Parser structure

--- PythonParser.py
+++ PythonParser.py
@@ -9,25 +9,30 @@ Last Updated Sep 24, 2024
 assumptions:
     -all relevant code of the codebase is in one root folder (neglects library functions for now)
     -there is a main execution file with a main function
-    -
+    -all code is executed within some function. The codebase has no lone code in a script or scripts.
 code flow:
-    1. reads through codebase and parse through every .py file to gather all function definitions of the codebase
+    1. reads through codebase and parse through every .py file to gather all function definitions
+        of the codebase by taking advantage of the keyword "def" in every function header.
         -stores this in a dictionary?
-            {"file1": [{"func1": ["sub_func1, sub_func2, ...],
-                        "func2": ["sub_func1, sub_func2, ...],
-                        ...
-                        }],
-             "file2": [{"func1": ["sub_func1, sub_func2, ...],
-                        "func2": ["sub_func1, sub_func2, ...],
-                        ...
-                        }],
+            ex1:
+            {"file1": ["func1", "func2", ...],
+             "file2": ["func1", "func2", ...],
              ...
             }
     2. finds the main execution file and its main function
-    3. create a second data structure that restructures and embeds the first structure into the second:
-        -starting at main, ou
-    4. outputs a sequential, hierarchical list of function calls that the
-        program execution runs through.
+    3. Parse through the main function, looking for likely function calls using regex and checking likely
+        functions against the known list of functions in the dictionary above. Function calls are recorded
+        in a second data structure, and the parser repeats by going into each of those function calls and
+        recording the function calls in them until all function calls have been parsed through.
+        -stores this in a dictionary:
+            {"main": {"func_call1": {"sub_func_call1:{}},
+                        "func_call2": {}
+                    }
+            }
+    4. outputs a sequential, hierarchical list of function calls that the program execution runs through,,
+        starting at the specified function.
+
+
 
 TODO:
 
@@ -37,11 +42,7 @@ TODO:
 import os
 import re
 
-def read_py(py_path):
-    with open(py_path, 'r') as file:
-        return file.read()
-
-# Extract function names from a file 
+# Extract function names from function headers in a file 
 def parse_py_function_names(py_file):
     function_names = []
     for line in py_file.split('\n'):
@@ -50,14 +51,35 @@ def parse_py_function_names(py_file):
             function_names.append(function_name)
     return function_names
 
+def read_py(py_file_path):
+    with open(py_file_path, 'r') as file:
+        return file.read()
+
+# Parse through entire codebase to create a list of all functions in the codebase
+# {"file1": ["func1", "func2", ...],
+#              "file2": ["func1", "func2", ...],
+#              ...
+#             }
+def parse_py_codebase_functions(py_codebase_path):
+    pass
+
+
 # Extract function calls inside a function
 def parse_py_function_calls(py_func):
     function_calls = []
-    for line in py_func:
-        if 
+    # need to identify the start and end of a function
+    # need to identify a possible function call
 
+def parse_py_codebase_execution(py_start_file, py_start_function, codebase_functions):
+    pass
+
+s
+def main():
+    py_codebase_path = ""
+    py_start_file = ""
+    py_start_function = ""
+    codebase_functions = parse_py_codebase_functions(py_codebase_path)
+    parse_py_codebase_execution(py_start_file, py_start_function, codebase_functions)
 
 if __name__ == "__main__":
-    py_path = "./TestPythonCodeBase/atmel_TestBed.py"
-    code = read_py(py_path)
-    print(type(code))
+    main()
